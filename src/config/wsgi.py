29ff9bfb833e9d68c3de33108623091bbba0a4ff
binit-singh7 <binits175@gmail.com>
2026-08-19 07:58:04 +0545

    Updated For Deployment

--- src/config/wsgi.py
+++ src/config/wsgi.py
@@ -8,9 +8,14 @@ https://docs.djangoproject.com/en/6.0/howto/deployment/wsgi/
 """
 
 import os
+import sys
+
+# Add the src directory to Python's path
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+sys.path.insert(0, BASE_DIR)
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings")
 
 application = get_wsgi_application()
